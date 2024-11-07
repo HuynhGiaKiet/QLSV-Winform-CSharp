40fff44a1e9e2a469908033a279296f8a51cf526
HuynhGiaKiet <kiethuynh1412@gmail.com>
2024-11-07 17:13:01 +0700

    UpFile

---
+++ QuanLySinhVien/ConnectSQL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    public class ConnectSQL
+    {
+        public static string connectionString = @"Data Source=DESKTOP-06D4HKR\SQLEXPRESS01;Initial Catalog=QuanLySinhVien;Integrated Security=True";
+    }
+}
